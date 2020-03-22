7fe158b1771305f0cdf306603ebeb70edf285564
Abbie <abbie@github.com>
2020-03-22 10:43:00 +0100

    First JavaScript task

---
+++ first_webpage/script.js
@@ -0,0 +1,11 @@
+const intern = "Abigail Nonyelim";
+console.log(intern);
+
+const courses = ["Frontend", "Mobile-Flutter"]
+console.log(courses);
+
+for (let i = 1; i < 200; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
